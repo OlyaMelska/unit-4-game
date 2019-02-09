7cc4e54e14afab5a3d191b364f0088afda4258c1
candye_sweet <olyamelska@Olhas-MBP.home>
2019-02-08 20:05:01 -0500

    Another one

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -1,38 +1,39 @@
 $(document).ready(function() {
-  //   let characters = [
-  //     {
-  //       name: "Jon Snow",
-  //       healthPoints: 200, //life length
-  //       attackPower: 20, // power of the hit, increases with each hit
-  //       counterAttackPower: 160 //
-  //     },
-  //     {
-  //       name: "Jaime Lannister",
-  //       healthPoints: 160, //life length
-  //       attackPower: 16, // power of the hit, increases with each hit
-  //       counterAttackPower: 110 //
-  //     },
-  //     {
-  //       name: "White Walker",
-  //       healthPoints: 220, //life length
-  //       attackPower: 22, // power of the hit, increases with each hit
-  //       counterAttackPower: 180 //
-  //     },
-  //     {
-  //       name: "Brienne of Tarth",
-  //       healthPoints: 180, //life length
-  //       attackPower: 18, // power of the hit, increases with each hit
-  //       counterAttackPower: 120 //
-  //     }
-  //   ];
-
   $(".characters").on("click", () => {
-    let chosenCharacter = $(this).val();
-    console.log(chosenCharacter);
-    $(chosenCharacter).appendTo("#chosenCharacter");
+    // $(".characters").attr("class", ".hide");
+    let $clickedCharacter = $(this).val();
+    $("#chosenCharacter").append($clickedCharacter);
+
+    console.log($clickedCharacter);
   });
 });
 
+//   let characters = [
+//     {
+//       name: "Jon Snow",
+//       healthPoints: 200, //life length
+//       attackPower: 20, // power of the hit, increases with each hit
+//       counterAttackPower: 160 //
+//     },
+//     {
+//       name: "Jaime Lannister",
+//       healthPoints: 160, //life length
+//       attackPower: 16, // power of the hit, increases with each hit
+//       counterAttackPower: 110 //
+//     },
+//     {
+//       name: "White Walker",
+//       healthPoints: 220, //life length
+//       attackPower: 22, // power of the hit, increases with each hit
+//       counterAttackPower: 180 //
+//     },
+//     {
+//       name: "Brienne of Tarth",
+//       healthPoints: 180, //life length
+//       attackPower: 18, // power of the hit, increases with each hit
+//       counterAttackPower: 120 //
+//     }
+//   ];
 //   $(characters).each((key, element) => {
 //     $(".names").text(element.name);
 //     console.log(element.name);
